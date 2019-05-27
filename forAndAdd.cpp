c0a0134d1871a9b334dac8290af01fe9a1aae289
jusondac <rejkamuhammad@gmail.com>
2019-05-27 16:53:13 +0700

    for has been added

---
+++ forAndAdd.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main(){
+	int N;
+	scanf("%d",&N);
+	int hasil = 0;
+	for(int i = 0; i < N; i++){
+		int Bi;
+		scanf("%d",&Bi);
+		hasil += Bi;
+	}
+	printf("%d\n", hasil);
+}
