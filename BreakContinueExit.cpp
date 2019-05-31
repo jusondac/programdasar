587cc720910b7e3f8ec71c8b732da748a400cfe5
jusondac <rejkamuhammad@gmail.com>
2019-05-31 13:12:23 +0700

    BreakContinueExit has been added

---
+++ BreakContinueExit.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int main(){
+	int a,b;
+	scanf("%d",&a);
+
+	for(b=1;b<=a;b++){
+		if(b % 10 == 0){
+			continue;
+		} else if (b == 42){
+			printf("ERROR\n");
+			break;
+		}
+		printf("%d\n",b);
+	}
+}
